60227616c941a6cf344cfd2cd901095d6de92641
Patricia Ciobanu <ciobanu.paty@gmail.com>
2019-03-15 17:11:07 +0100

    Style changes

--- portfolio/src/components/project-list.js
+++ portfolio/src/components/project-list.js
@@ -1,59 +1,57 @@
 import React from 'react';
 
-const projectData = 
-{
-    projects:[
-      {
-        id: 1,
-        img: require('../img/travel-australia.jpg'),
-        title: 'Travel to Australia',
-        name: 'travel-australia',
-        year: '2016'
-      },
-      {
-        id: 2,
-        img: require('../img/panc.jpg'),
-        title: 'PANC',
-        name: 'panc',
-        year: '2017'
-      },
-      {
-        id: 3,
-        img: require('../img/synkroma2.jpg'),
-        title: 'Synkroma',
-        name: 'synkroma',
-        year: '2017'
-      },
-      {
-        id: 4,
-        img: require('../img/lumobok.jpg'),
-        title: 'Lumobok',
-        name: 'lumobok',
-        year: '2017'
-      },
-      {
-        id: 5,
-        img: require('../img/interactive-tree-1.jpg'),
-        title: 'Interactive Tree',
-        name: 'interactive-tree',
-        year: '2018'
-      },
-      {
-        id: 6,
-        img: require('../img/sentify.png'),
-        title: 'Sentify',
-        name: 'sentify',
-        year: '2018'
-      },
-      {
-        id: 7,
-        img: require('../img/into-the-woods.png'),
-        title: 'Into The Woods',
-        name: 'into-the-woods',
-        year: '2018'
-      }
-    ]
-};
+const projects = 
+[
+  {
+    id: 1,
+    img: require('../img/travel-australia.jpg'),
+    title: 'Travel to Australia',
+    name: 'travel-australia',
+    year: '2016'
+  },
+  {
+    id: 2,
+    img: require('../img/panc.jpg'),
+    title: 'PANC',
+    name: 'panc',
+    year: '2017'
+  },
+  {
+    id: 3,
+    img: require('../img/synkroma2.jpg'),
+    title: 'Synkroma',
+    name: 'synkroma',
+    year: '2017'
+  },
+  {
+    id: 4,
+    img: require('../img/lumobok.jpg'),
+    title: 'Lumobok',
+    name: 'lumobok',
+    year: '2017'
+  },
+  {
+    id: 5,
+    img: require('../img/interactive-tree-1.jpg'),
+    title: 'Interactive Tree',
+    name: 'interactive-tree',
+    year: '2018'
+  },
+  {
+    id: 6,
+    img: require('../img/sentify.png'),
+    title: 'Sentify',
+    name: 'sentify',
+    year: '2018'
+  },
+  {
+    id: 7,
+    img: require('../img/into-the-woods.png'),
+    title: 'Into The Woods',
+    name: 'into-the-woods',
+    year: '2018'
+  }
+];
 
 function ProjectGridList(props) {
   return (
@@ -61,13 +59,10 @@ function ProjectGridList(props) {
       {/*Main*/}
       <div id="main">
         <section id="one" className="tiles">
-          {projectData.projects.map(project => (
-            <article key={project.id} style={{backgroundImage: "url('" + project.img +"')"}}>
-              <span className="image">
-                <img src={project.img} alt={project.title} />
-              </span>
+          {projects.reverse().map(project => (
+            <article key={project.id} style={{backgroundImage: "url('" + project.img +"')"}} onClick={event => props.handleRedirection(event, project.name)}>
               <header className="major">
-                <h3><a href="#" onClick={event => props.handleRedirection(event, project.name)} className="link">{project.title}</a></h3>
+                <h3>{project.title}</h3>
               </header>
             </article>
           ))}
