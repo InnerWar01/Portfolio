246921afb120edb99de0e0c1f87bd0a490daed39
InnerWar01 <ciobanu.paty@gmail.com>
2020-09-29 16:01:50 +0200

    Updates

---
+++ precache-manifest.cf88be8f7a51b5e0d9dc5b618118dc83.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051a4340ed1a7223a33cfded146c86c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "922f66228eed08aa6ee3",
+    "url": "/portfolio/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "1f40e397684629cfbcbc",
+    "url": "/portfolio/static/css/main.33d30a6d.chunk.css"
+  },
+  {
+    "revision": "922f66228eed08aa6ee3",
+    "url": "/portfolio/static/js/2.7144ac03.chunk.js"
+  },
+  {
+    "revision": "72fd966c032a7190c384f9c42f4c5eea",
+    "url": "/portfolio/static/js/2.7144ac03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f40e397684629cfbcbc",
+    "url": "/portfolio/static/js/main.06ec3643.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7576c9129949fb191ebb7b87904b932d",
+    "url": "/portfolio/static/media/avatar.7576c912.jpg"
+  },
+  {
+    "revision": "a5607182841f20ba8decd55f8e3597bc",
+    "url": "/portfolio/static/media/interactive-tree-1.a5607182.jpg"
+  },
+  {
+    "revision": "e210a8932bfa632611ce2eba7e8f69ff",
+    "url": "/portfolio/static/media/interactive-tree-2.e210a893.jpg"
+  },
+  {
+    "revision": "eb2fbc9c8aa9a5e8162c9a7d05983de9",
+    "url": "/portfolio/static/media/interactive-tree-3.eb2fbc9c.jpg"
+  },
+  {
+    "revision": "3c65939208b3f059e86f8dbeb300bd47",
+    "url": "/portfolio/static/media/interactive-tree-4.3c659392.jpg"
+  },
+  {
+    "revision": "3b0d611b091ab077e090fd810c4647c0",
+    "url": "/portfolio/static/media/interactive-tree-5.3b0d611b.jpg"
+  },
+  {
+    "revision": "49b75c05e1250ad927783c617db38afb",
+    "url": "/portfolio/static/media/into-the-woods.49b75c05.png"
+  },
+  {
+    "revision": "1e3e47edf148467cc37181f9c7abf2fa",
+    "url": "/portfolio/static/media/into-the-woods1.1e3e47ed.jpg"
+  },
+  {
+    "revision": "c28dea06133ad1d130cc3e062e5a3ff7",
+    "url": "/portfolio/static/media/into-the-woods2.c28dea06.jpg"
+  },
+  {
+    "revision": "bfb08e423dbf3a44571e864eaff21af0",
+    "url": "/portfolio/static/media/into-the-woods3.bfb08e42.jpg"
+  },
+  {
+    "revision": "420a39becabeb1074e98d1e61f402003",
+    "url": "/portfolio/static/media/into-the-woods4.420a39be.gif"
+  },
+  {
+    "revision": "ef674081b8bdfece788ca94a5deacc15",
+    "url": "/portfolio/static/media/into-the-woods5.ef674081.jpg"
+  },
+  {
+    "revision": "93937b0b4250e3e5a0a4ab5bf4572b8a",
+    "url": "/portfolio/static/media/into-the-woods6.93937b0b.jpg"
+  },
+  {
+    "revision": "65f9ceafdb2188338550a668e077d592",
+    "url": "/portfolio/static/media/into-the-woods7.65f9ceaf.jpg"
+  },
+  {
+    "revision": "b1050168537245bc1cc2f9f1c454367b",
+    "url": "/portfolio/static/media/lumo1.b1050168.gif"
+  },
+  {
+    "revision": "44b33f8c092cab8b8245292bd883f3fd",
+    "url": "/portfolio/static/media/lumo2.44b33f8c.gif"
+  },
+  {
+    "revision": "604e8c774263358adfdc6d137de45493",
+    "url": "/portfolio/static/media/lumo3.604e8c77.gif"
+  },
+  {
+    "revision": "259bb9c7b57fb69fc3ed8e52b496cd6f",
+    "url": "/portfolio/static/media/lumo4.259bb9c7.gif"
+  },
+  {
+    "revision": "d00b4c90d1a081629fd6532bf517965f",
+    "url": "/portfolio/static/media/lumo5.d00b4c90.gif"
+  },
+  {
+    "revision": "bf2ac0673bcbd5eff4b12b487bbae6dc",
+    "url": "/portfolio/static/media/lumobok.bf2ac067.jpg"
+  },
+  {
+    "revision": "e3900c601fc68c29c14c2f760bb9c376",
+    "url": "/portfolio/static/media/master-thesis.e3900c60.jpg"
+  },
+  {
+    "revision": "b6bdc1b216bda1760c339d8a7ca0ac5f",
+    "url": "/portfolio/static/media/master-thesis1.b6bdc1b2.gif"
+  },
+  {
+    "revision": "72f31a714cbe93bf5643715f77c27f3e",
+    "url": "/portfolio/static/media/panc.72f31a71.jpg"
+  },
+  {
+    "revision": "68d693c948148a6a95a9f568eeea4845",
+    "url": "/portfolio/static/media/panc1.68d693c9.gif"
+  },
+  {
+    "revision": "f32c25b934a8b30187c24145c3747cf6",
+    "url": "/portfolio/static/media/panc2.f32c25b9.gif"
+  },
+  {
+    "revision": "530d84efb0e628203272830c8c78b8e5",
+    "url": "/portfolio/static/media/panc3.530d84ef.gif"
+  },
+  {
+    "revision": "0d1dc270eb9a23d7b3fa7fc9f636c0d3",
+    "url": "/portfolio/static/media/panc4.0d1dc270.gif"
+  },
+  {
+    "revision": "056997c270bfa29b0b999ceab6430319",
+    "url": "/portfolio/static/media/panc5.056997c2.gif"
+  },
+  {
+    "revision": "fe2c7458ec6e902989b159647e237401",
+    "url": "/portfolio/static/media/sentify.fe2c7458.png"
+  },
+  {
+    "revision": "8d46a34a2009a0e4d0460010158ae5c9",
+    "url": "/portfolio/static/media/sentify1.8d46a34a.gif"
+  },
+  {
+    "revision": "df2d9aa504246aeb70b8af4d95b59ee0",
+    "url": "/portfolio/static/media/sentify2.df2d9aa5.gif"
+  },
+  {
+    "revision": "6af8c6d1b330e1d26764392b3ea70b06",
+    "url": "/portfolio/static/media/sentify3.6af8c6d1.gif"
+  },
+  {
+    "revision": "edefd485abeaff7635b63758391aa983",
+    "url": "/portfolio/static/media/sentify4.edefd485.gif"
+  },
+  {
+    "revision": "0f30bbb9b31f6d235e52810befccf349",
+    "url": "/portfolio/static/media/synkroma1.0f30bbb9.jpg"
+  },
+  {
+    "revision": "fbaf0bd5521acb48670dce44e6252066",
+    "url": "/portfolio/static/media/synkroma2.fbaf0bd5.jpg"
+  },
+  {
+    "revision": "e1ed96b2c494cf92f54c4bdb96b3b261",
+    "url": "/portfolio/static/media/synkroma3.e1ed96b2.jpg"
+  },
+  {
+    "revision": "7326c28aba5c21a5b4285d8fe52b7ba3",
+    "url": "/portfolio/static/media/synkroma4.7326c28a.jpg"
+  },
+  {
+    "revision": "8dc1f427c1fb7ad75a35cb2909270623",
+    "url": "/portfolio/static/media/synkroma5.8dc1f427.gif"
+  },
+  {
+    "revision": "a3805cc7211fdb8bb63150b84964a586",
+    "url": "/portfolio/static/media/synkroma6.a3805cc7.gif"
+  },
+  {
+    "revision": "df39263a29e2c7fd909be78f673741d5",
+    "url": "/portfolio/static/media/synkromalogo.df39263a.png"
+  },
+  {
+    "revision": "fd9c825a0ec5fc5897fc64a97ff99d4f",
+    "url": "/portfolio/static/media/travel-australia.fd9c825a.jpg"
+  },
+  {
+    "revision": "fcb5d10b52bd740ca413036fb496701f",
+    "url": "/portfolio/static/media/travel-australia1.fcb5d10b.gif"
+  },
+  {
+    "revision": "b0c6b89b9af4ae11b5b86d5d415314ca",
+    "url": "/portfolio/static/media/travel-australia2.b0c6b89b.gif"
+  },
+  {
+    "revision": "e70690984e4333dbd2d12a54a43dd6c7",
+    "url": "/portfolio/static/media/travel-australia3.e7069098.gif"
+  },
+  {
+    "revision": "dce0e98ef7ce833a8d897b682dc77e22",
+    "url": "/portfolio/static/media/travel-australia4.dce0e98e.gif"
+  },
+  {
+    "revision": "cfd0f29b1daefe5c427f3e3dfc5f3c4c",
+    "url": "/portfolio/static/media/travel-australia5.cfd0f29b.gif"
+  },
+  {
+    "revision": "65e605b81c486bf588e9d9e137c93eca",
+    "url": "/portfolio/static/media/travel-australia6.65e605b8.gif"
+  }
+]);
